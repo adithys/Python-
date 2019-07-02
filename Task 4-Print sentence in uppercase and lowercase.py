7669eb727d9ffd055e82c5c25f701d04a6afbe7a
adithys <51487891+adithys@users.noreply.github.com>
2019-07-02 11:08:32 +0400

    Add files via upload

---
+++ Task 4-Print sentence in uppercase and lowercase.py
@@ -0,0 +1,5 @@
+#Adithya Krishnan
+#Python task4 Accept a string snd print in both lowercase and uppercase
+A=input("Enter a string: ")
+print("Uppercase: ", A.upper())
+print("Lowercase: ", A.lower())
