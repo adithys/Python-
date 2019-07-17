e9fb01b31225886519024f32c02ce08c07f80a65
adithys <51487891+adithys@users.noreply.github.com>
2019-07-17 18:48:49 +0400

    Add files via upload

---
+++ Task 10-Using functions find area of rectangle.py
@@ -0,0 +1,12 @@
+#Adithya Krishnan
+#Task 10-Print area of rectangle using functions
+class Rectangle():
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
+
+    def rectangle_area(self):
+        return self.length*self.width
+
+newRectangle = Rectangle(12, 10)
+print(newRectangle.rectangle_area())
